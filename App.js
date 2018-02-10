cece4adada1eabac92d4468bedee47cd4e339f5f
sonicsmith <thesonicsmith@gmail.com>
2018-02-10 22:05:39 +1300

    Initial Commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import App from './lib/App';
+
+export default class Wrapper extends React.Component {
+  render() {
+    return (
+      <App/>
+    );
+  }
+}
---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import * as React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default class App extends React.Component {
+  public render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    backgroundColor: "#fff",
+    flex: 1,
+    justifyContent: "center",
+  },
+});
